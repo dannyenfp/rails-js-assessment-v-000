d68b4b3d5f6722d415cc91a8fad9bf3f3ed3ef2e
dannyenfp <daniel.valmas@gmail.com>
2016-11-02 07:49:56 -0400

    added workout form

--- workout_app/db/migrate/20161031164734_create_workouts.rb
+++ workout_app/db/migrate/20161031164734_create_workouts.rb
@@ -2,10 +2,12 @@ class CreateWorkouts < ActiveRecord::Migration[5.0]
   def change
     create_table :workouts do |t|
       t.datetime :date
+      t.string :description
       t.string :workout
       t.string :duration
       t.string :location
       t.string :mood
+      t.integer :user_id
 
 
       t.timestamps
