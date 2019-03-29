abe6b0095ec948882f5458296bb77c6f70bebac2
dannyenfp <daniel.valmas@gmail.com>
2019-03-29 14:03:42 -0700

    Begin work to change the workouts to be rendered with JSON

--- workout_app/app/controllers/workouts_controller.rb
+++ workout_app/app/controllers/workouts_controller.rb
@@ -4,6 +4,15 @@ class WorkoutsController < ApplicationController
 #this becomes confusing when all the routes are in the manner of users/:id/workouts
 #how should i structure this, and this was almost impossible
 
+##Begin to this: 
+# Must render at least one index page (index resource - 'list of things') 
+# via JavaScript and an Active Model Serialization JSON Backend.
+
+# For example: in a blog domain with users and posts, 
+# you might display the index of the user's 
+# posts on the users show page, fetching the posts via a AJAX GET request, with the backend rendering the posts in JSON format, and then appending the posts to the page.
+
+
 
   def index
     if params[:user_id]
